a1456ef96a4724d89304f595d35b86b071125359
wishyduh <dmbres23@gmail.com>
2023-02-05 13:43:45 -0500

    Files for backend
    This is part of the backend that was presented in the tutorial, working on getting more of it cleaned up

---
+++ movies/movies/src/main/java/dev/wishyduh/movies/ReviewService.java
@@ -0,0 +1,31 @@
+package dev.wishyduh.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReviewB(String reviewBody, String imdbId){
+       Review review = reviewRepository.insert(new Review(reviewBody));
+
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
